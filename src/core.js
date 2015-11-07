5d49eceb372c1ca596e3d66a821dd5a584be9394
Evgeniy Kazakov <evgeniy.kazakov@gmail.com>
2015-11-08 00:59:05 +0300

    add reducer

--- src/core.js
+++ src/core.js
@@ -1,11 +1,25 @@
 import {List, Map, fromJS} from 'immutable';
 
+export const INITIAL_STATE = Map();
+
 export function setEntries(state, entries) {
     return state.set('entries', List(entries));
 }
 
 export function next(state) {
-    const entries = state.get('entries');
+    const entries = state
+        .get('entries')
+        .concat(getWinners(state.get('vote')))
+    ;
+
+    if (entries.size === 1) {
+        return state
+            .remove('vote')
+            .remove('entries')
+            .set('winner', entries.first())
+        ;
+    }
+
     return state.merge({
         vote: Map({pair: entries.take(2)}),
         entries: entries.skip(2)
@@ -13,17 +27,22 @@ export function next(state) {
 }
 
 export function vote(state, item) {
-    //const tally = state.getIn(['vote', 'tally'], fromJS({tally: {}}));
-    //let votesForItem = tally.get(item, 0);
-    //return state.mergeDeepIn(['vote'], fromJS({
-    //    tally: {
-    //        [item]: votesForItem + 1
-    //    }
-    //}));
-
     return state.updateIn(
         ['vote', 'tally', item],
         0,
         tally => tally + 1
     )
+}
+
+function getWinners(vote) {
+    if (!vote) return [];
+
+    const [a,b] = vote.get('pair');
+    const aVotes = vote.getIn(['tally', a], 0);
+    const bVotes = vote.getIn(['tally', b], 0);
+
+    if (aVotes > bVotes) return [a];
+    if (aVotes < bVotes) return [b];
+
+    return [a, b];
 }
